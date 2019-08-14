a96c0ff783c4d6b0fb5f4a6de8d5420c9f413b61
hplus-support <hplus-support@cgiar.org>
2019-08-14 17:46:11 +0200

    producer kafka

--- src/services/kafka/producer.js
+++ src/services/kafka/producer.js
@@ -2,26 +2,32 @@ const kafka = require('kafka-node');
 //const bp = require('body-parser');
 const config = require('./config');
 
-module.exports.sendDataToBroker= function(data,topic){
+//module.exports.sendDataToBroker= function(data,topic){
     try {
     const Producer = kafka.Producer;
     const client = new kafka.KafkaClient({kafkaHost: config.kafka_server});
     const producer = new Producer(client);
-    const kafka_topic=  topic//config.kafka_topic;
+    const kafka_topic=  config.kafka_topic;
     const kafka_message = 'I am since nodejs';
 
+    client.on('error',(err) => {
+        console.log(err)
+    })
     //const complexMessage= JSON.stringify({hola:'callado', felipe:'felipe'})
     console.log(kafka_message);
     let payloads = [
         {
         topic: kafka_topic,
-        messages: data
+        messages: [kafka_message]
         }
     ];
+
+    console.log(config.kafka_server,payloads)
 
     producer.on('ready', async function() {
         let push_status = producer.send(payloads, (err, data) => {
         if (err) {
+            console.error(err)
             console.log('[kafka-producer -> '+kafka_message+']: broker update failed');
         } else {
             console.log('[kafka-producer -> '+kafka_message+']: broker update success');
@@ -38,4 +44,4 @@ module.exports.sendDataToBroker= function(data,topic){
     catch(e) {
     console.log(e);
     }
-}
+//}
