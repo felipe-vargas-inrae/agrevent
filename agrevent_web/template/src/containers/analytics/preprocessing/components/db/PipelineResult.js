3a774d3981668c938ee6074b9dd245635c13b3e2
hplus-support <hplus-support@cgiar.org>
2019-11-26 17:01:55 +0100

    preprocessing joiner and constans

---
+++ agrevent_web/template/src/containers/analytics/preprocessing/components/db/PipelineResult.js
@@ -0,0 +1,24 @@
+const data = [
+    { "Biomass(gramos_pesofresco)": "327", "Treatment": "WD" }, 
+    { "Biomass(gramos_pesofresco)": "345", "Treatment": "WD" }, 
+    { "Biomass(gramos_pesofresco)": "310", "Treatment": "WD" }, 
+    { "Biomass(gramos_pesofresco)": "250", "Treatment": "WD" }, 
+    { "Biomass(gramos_pesofresco)": "243", "Treatment": "WD" }, 
+    { "Biomass(gramos_pesofresco)": "264", "Treatment": "WD" }, 
+    { "Biomass(gramos_pesofresco)": "264", "Treatment": "WD" }, 
+    { "Biomass(gramos_pesofresco)": "339", "Treatment": "WD" }, 
+    { "Biomass(gramos_pesofresco)": "245", "Treatment": "WD" }, 
+    { "Biomass(gramos_pesofresco)": "299", "Treatment": "WD" }, 
+    { "Biomass(gramos_pesofresco)": "203", "Treatment": "WD" }, 
+    { "Biomass(gramos_pesofresco)": "256", "Treatment": "WD" }, 
+    { "Biomass(gramos_pesofresco)": "281", "Treatment": "WD" }, 
+    { "Biomass(gramos_pesofresco)": "238", "Treatment": "WD" }, 
+    { "Biomass(gramos_pesofresco)": "299", "Treatment": "WD" }, 
+    { "Biomass(gramos_pesofresco)": "292", "Treatment": "WD" }, 
+    { "Biomass(gramos_pesofresco)": "338", "Treatment": "WD" }, 
+    { "Biomass(gramos_pesofresco)": "293", "Treatment": "WD" }, 
+    { "Biomass(gramos_pesofresco)": "223", "Treatment": "WD" }, 
+    { "Biomass(gramos_pesofresco)": "243", "Treatment": "WD" }]
+
+
+export default data
---
+++ agrevent_web/template/src/containers/analytics/preprocessing/constans.js
@@ -0,0 +1,5 @@
+
+
+export const   API_SPARK="http://127.0.0.1:5000"
+export const   API_DATAFRAMES=API_SPARK+"/api/v1/resources/dataframes"
+export const   API_PREPROCESSING_PIPELINES=API_DATAFRAMES+"/preprocessing_pipelines"
