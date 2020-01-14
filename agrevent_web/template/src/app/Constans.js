2d7ca04f3e06f486603c5d7ee469d4e4dd669b8d
hplus-support <hplus-support@cgiar.org>
2020-01-14 18:06:39 +0100

    constans

---
+++ agrevent_web/template/src/app/Constans.js
@@ -0,0 +1,6 @@
+
+export const   API_SPARK="http://localhost:5000"
+export const   API_DATAFRAMES=API_SPARK+"/api/v1/resources/dataframes"
+
+export const API_JOINER_CORRELATIONS= API_SPARK + '/api/v1/resources/joiner/correlations'
+export const   API_PREPROCESSING_PIPELINES=API_DATAFRAMES+"/preprocessing_pipelines"
